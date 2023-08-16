ac832ed4ca5fa961782c88d3eeb1e0ec8d44576f
adnan007d <adnanmansuri54@gmail.com>
2023-08-16 17:37:06 +0530

    seperated undotree, trouble, harpoon and added prettier

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,4 @@
 return {
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
-  "theprimeagen/harpoon",
-  "mbbill/undotree",
   "tpope/vim-fugitive",
   "nvim-treesitter/nvim-treesitter-context",
 }
---
+++ lua/plugins/prettier.lua
@@ -0,0 +1,5 @@
+return {
+  'adnan007d/vim-prettier',
+  build = "yarn install",
+  ft = { 'javascript', 'typescript', 'css', 'markdown', 'html', 'javascriptreact', 'typescriptreact', 'json' }
+}
