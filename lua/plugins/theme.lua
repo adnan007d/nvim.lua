e92b88a6b3161058002ec3cd2c7d79e98be6511b
adnan007d <adnanmansuri54@gmail.com>
2023-08-17 13:02:58 +0530

    changing the position of theme setting

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,11 @@
 return {
   'Mofiqul/dracula.nvim',
   config = function(_)
+    vim.cmd.colorscheme("dracula")
     if (jit.os ~= "Windows") then
       require("dracula").setup({ disable_background = true })
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
-    vim.cmd.colorscheme("dracula")
   end
 }
