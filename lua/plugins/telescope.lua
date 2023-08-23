957f3ab37534e9b07d6f0a602e9602f50ec0b582
adnan007d <adnanmansuri54@gmail.com>
2023-08-23 20:45:49 +0530

    added telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,9 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.2',
   -- or                              , branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
   config = function(_)
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
@@ -11,5 +13,17 @@ return {
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end)
     vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+    if vim.fn.executable("make") == 1 then
+      require("telescope").setup {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+        }
+      }
+
+      require("telescope").load_extension("fzf")
+    end
   end
 }
