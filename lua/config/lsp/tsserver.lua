73f19b469ba7bfc4f47751afa4a733603f820bb6
adnan007d <adnanmansuri54@gmail.com>
2023-09-17 01:33:14 +0530

    capabilites updated

--- lua/config/lsp/tsserver.lua
+++ lua/config/lsp/tsserver.lua
@@ -1,5 +1,8 @@
 return function()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
   require('lspconfig').tsserver.setup({
+    capabilities = capabilities,
     on_init = function(client)
       client.server_capabilities.documentFormattingProvider = false
       client.server_capabilities.documentFormattingRangeProvider = false
