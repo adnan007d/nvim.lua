187602eaaba936cb0556c1c3e79cf5d8bdcd1792
adnan007d <adnanmansuri54@gmail.com>
2024-11-28 02:39:41 +0000

    chore: stable lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -133,25 +133,25 @@ return {
       })
 
 
-      -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-      --   vim.lsp.handlers.hover,
-      --   { border = "rounded" }
-      -- )
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover,
+        { border = "rounded" }
+      )
 
 
-      -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf.with(
-      --   vim.lsp.buf.handlers.signature_help,
-      --   { border = "rounded" }
-      -- )
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+        vim.lsp.handlers.signature_help,
+        { border = "rounded" }
+      )
 
 
-      vim.lsp.buf.hover({
-        border = "rounded"
-      })
-
-      vim.lsp.buf.signature_help({
-        border = "rounded"
-      })
+      -- vim.lsp.buf.hover({
+      --   border = "rounded"
+      -- })
+      --
+      -- vim.lsp.buf.signature_help({
+      --   border = "rounded"
+      -- })
 
       vim.diagnostic.config({
         virtual_text = true,
