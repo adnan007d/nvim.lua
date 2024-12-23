17cb1036cebb2786709514664573d5818db30325
adnan007d <adnanmansuri54@gmail.com>
2024-12-23 04:48:07 +0000

    feat: cs formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,32 @@
 return {
-  'stevearc/conform.nvim',
-  config = function()
-    local prettier_fts = { "javascript", "javascriptreact", "typescript", "typescriptreact", "markdown", "vue", "css",
-      "json", "html" }
+	"stevearc/conform.nvim",
+	config = function()
+		local prettier_fts = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"markdown",
+			"vue",
+			"css",
+			"json",
+			"html",
+		}
 
+		local formatters_by_ft = {
+			lua = { "stylua" },
+			cs = { "csharpier" },
+		}
 
-    local formatters_by_ft = {
-      lua = { "stylua" },
-    }
+		for _, ft in ipairs(prettier_fts) do
+			formatters_by_ft[ft] = { "prettier", "prettier", stop_after_first = true }
+		end
 
-    for _, ft in ipairs(prettier_fts) do
-      formatters_by_ft[ft] = { "prettier", "prettier", stop_after_first = true }
-    end
-
-    require('conform').setup({
-      formatters_by_ft = formatters_by_ft,
-      default_format_opts = {
-        lsp_format = "fallback",
-      },
-    })
-  end
+		require("conform").setup({
+			formatters_by_ft = formatters_by_ft,
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+		})
+	end,
 }
